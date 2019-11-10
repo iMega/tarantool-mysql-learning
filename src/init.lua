450379731b5f121545ca9388217ac3d68f81fb85
Dmitry <irvis@imega.ru>
2019-11-10 19:51:05 +0300

    up

--- src/init.lua
+++ src/init.lua
@@ -1,6 +1,9 @@
 local log = require 'log'
 local inspect = require 'inspect'
+local fiber = require 'fiber'
 local mysql = require('mysql')
+
+local channels = {http_in = fiber.channel(10)}
 
 box.cfg{log_format = 'json'}
 
@@ -54,6 +57,56 @@ print(inspect(err))
 local d = box.space.articles:select{100500}
 log.info("===FROM TUPLE" .. inspect(d))
 
--- local httpd = require('http.server').new('0.0.0.0', 9000, {})
--- httpd:route({path = '/dictionary/:record_name', method = 'GET'}, get_dictionary_record_handler)
--- httpd:start()
+local writerDB
+writerDB = fiber.create(function()
+    local msg
+    local buffer = ""
+    while true do
+        log.info("===== while true do START len=" .. buffer:len())
+
+        log.info("===== 65")
+
+        if buffer:len() == 0 then
+            log.info("===== INFINITY")
+            msg = channels.http_in:get()
+        else
+            log.info("===== TIMEOUT 15")
+            msg = channels.http_in:get(15)
+        end
+
+        log.info("===== 73")
+
+        if msg == nil then
+            log.info("===== ENDING send to DB ")
+            buffer = ""
+            -- writerDB.
+            fiber.yield()
+        else
+            buffer = buffer .. "+" .. msg
+            log.info("===== BUFFER: " .. buffer:len())
+        end
+
+        if buffer:len() >= 15 then
+            log.info("===== send to DB ")
+            buffer = ""
+        end
+
+        log.info("===== while true do END")
+    end
+end)
+
+function http_handler(ctx)
+    log.info("count fibers " .. writerDB.status())
+    local ok = channels.http_in:put("test")
+    return {status = 200, body = 'test' .. inspect(ok)}
+end
+
+function http_handler2(ctx)
+    log.info("http_handler2 = " .. writerDB.status())
+    return {status = 200, body = 'test'}
+end
+
+local httpd = require('http.server').new('0.0.0.0', 9000, {})
+httpd:route({path = '/', method = 'GET'}, http_handler)
+httpd:route({path = '/2', method = 'GET'}, http_handler2)
+httpd:start()
