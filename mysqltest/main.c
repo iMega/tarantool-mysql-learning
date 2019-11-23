a5c9355e1ad0a6d53f85d96cee4fef21e4482f33
Dmitry <irvis@imega.ru>
2019-11-23 15:46:20 +0300

    fetch records over stmt

--- mysqltest/main.c
+++ mysqltest/main.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 #include <mysql/mysql.h>
 
+#define true 1
+
 int main(char **args)
 {
     MYSQL_RES *result;
     MYSQL_ROW row;
     MYSQL *connection, mysql;
-    int state;
+
+    int state, err = 0;
 
     mysql_init(&mysql);
     connection = mysql_real_connect(&mysql,
@@ -25,20 +28,114 @@ int main(char **args)
         return 1;
     }
 
-    state = mysql_query(connection, "select `f_longtext` from mytable where pri = 2");
-    if (state != 0)
+    MYSQL_STMT *stmt = mysql_stmt_init(&mysql);
+    if (!stmt)
     {
-        fprintf(stderr, "%s\n", mysql_error(connection));
+        fprintf(stderr, "%s\n", mysql_error(&mysql));
         return 1;
     }
 
-    result = mysql_store_result(connection);
-    while ((row = mysql_fetch_row(result)) != NULL)
+    const char *query = "select `f_longtext_null` from mytable where pri = ?";
+    err = mysql_stmt_prepare(stmt, query, 51);
+    if (err)
     {
+        fprintf(stderr, "failed to prepare query, %s\n", mysql_stmt_error(stmt));
+        return 1;
+    }
+
+    MYSQL_BIND *param_binds = NULL;
+
+    uint64_t *values = NULL;
+    unsigned long paramCount = mysql_stmt_param_count(stmt);
+    param_binds = (MYSQL_BIND *)calloc(sizeof(*param_binds), paramCount);
+    values = (uint64_t *)calloc(sizeof(*values), paramCount);
+
+    int val = 1;
+
+    param_binds[0].buffer_type = MYSQL_TYPE_LONG;
+    param_binds[0].buffer_length = 8;
+    param_binds[0].buffer = (char *)&val;
+
+    mysql_stmt_bind_param(stmt, param_binds);
+
+    err = mysql_stmt_execute(stmt);
+    if (err)
+    {
+        fprintf(stderr, "failed to execute query, %s\n", mysql_stmt_error(stmt));
+        return 1;
+    }
+
+    MYSQL_RES *meta = mysql_stmt_result_metadata(stmt);
+    if (!meta)
+    {
+        fprintf(stderr, "failed getting metadata, %s\n", mysql_stmt_error(stmt));
+        return 1;
+    }
+
+    unsigned long col_count = mysql_num_fields(meta);
+    MYSQL_BIND *result_binds = (MYSQL_BIND *)calloc(sizeof(MYSQL_BIND), col_count);
+    MYSQL_FIELD *fields = mysql_fetch_fields(meta);
+
+    unsigned long col_no;
+    for (col_no = 0; col_no < col_count; ++col_no)
+    {
+        result_binds[col_no].buffer_type = MYSQL_TYPE_STRING;
+        result_binds[col_no].buffer = (char *)malloc(fields[col_no].length);
+        result_binds[col_no].buffer_length = fields[col_no].length;
+        result_binds[col_no].length = (unsigned long *)malloc(sizeof(unsigned long));
+        result_binds[col_no].is_null = (my_bool *)malloc(sizeof(my_bool));
+    }
+    mysql_stmt_bind_result(stmt, result_binds);
+
+    if (mysql_stmt_store_result(stmt))
+    {
+        fprintf(stderr, "failed to store results %s\n", mysql_stmt_error(stmt));
+        return 1;
+    }
+
+    int row_count = 0;
+    fprintf(stdout, "Fetching results ...\n");
+    while (!mysql_stmt_fetch(stmt))
+    {
+        row_count++;
+        fprintf(stdout, "  row %d\n", row_count);
+
+        fprintf(stdout, "  COL: %s\n", result_binds[row_count].buffer);
+    }
+
+    // for (col_no = 0; col_no < col_count; ++col_no)
+    // {
+    //     if (*results[col_no].is_null)
+    //         continue;
+    //     lua_pushstring(L, fields[col_no].name);
+    //     lua_mysql_push_value(L, fields + col_no,
+    //                          results[col_no].buffer,
+    //                          *results[col_no].length);
+    //     lua_settable(L, -3);
+    // }
+
+    // state = mysql_query(connection, "select `f_longtext_null` from mytable where pri = 2");
+    // if (state != 0)
+    // {
+    //     fprintf(stderr, "%s\n", mysql_error(connection));
+    //     return 1;
+    // }
+
+    result = mysql_store_result(connection);
+
+    do
+    {
+        row = mysql_fetch_row(result);
+        if (!row)
+        {
+            break;
+        }
+
         printf("id: %s, val: %s\n",
                (row[0] ? row[0] : "NULL"),
                (row[1] ? row[1] : "NULL"));
-    }
+
+    } while (true);
 
     mysql_free_result(result);
     mysql_close(connection);
